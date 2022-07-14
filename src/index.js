c42b3752ef01a26a2e0ccac8b0080ed043d62026
AlmondChips <chips6t@gmail.com>
2022-07-14 18:18:56 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
+	let pureNum = "";
+	if (n.toString().includes("-"))
+	{
+	  pureNum += n.toString().substring(1);
   
+	  pureNum = pureNum.toString().split("").reverse().join("");
+	  
+	  return pureNum;
+	}
+	else{
+	  
+	  
+	  pureNum = n.toString().split("").reverse().join("");
+	  
+	  return Number(pureNum);
+	}
+  
+   
+  
+	
 }
